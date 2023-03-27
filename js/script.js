81f187b080dfe1d6a0d99271cbca37afd5669483
Arturo Valencia <10554086+arturovz23@users.noreply.github.com>
2023-03-27 16:58:59 -0700

    Creacion de navbar y del apartado Hero.

---
+++ js/script.js
@@ -0,0 +1,134 @@
+//VARIABLES
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const darkLight = document.querySelector('.darklight');
+const btnOpenMenu = document.querySelector('.openMenu'); 
+const btnCloseMenu = document.querySelector('.closeMenu');
+
+let getMode = localStorage.getItem('mode'); //get items from local storage
+
+if (getMode && getMode === 'dark-mode'){
+    body.classList.add('dark');
+}
+
+
+
+//EVENT LISTENERS
+
+//JS CODE TO TOGGLE DARK AND LIGHT MODE
+darkLight.addEventListener('click', () =>{
+    darkLight.classList.toggle('active');
+    body.classList.toggle('dark');
+
+    //JS CODE TO KEEP USER SELECTED MODE EVEN PAGE REFRESH
+    if (!body.classList.contains('dark')) {
+        localStorage.setItem('mode', 'light-mode'); //Add the key and value
+    }else{
+        localStorage.setItem('mode', 'dark-mode');
+    }
+})
+
+//JS CODE TO TOGGLE MOVIL MENU
+btnOpenMenu.addEventListener('click', () =>{
+    nav.classList.add("activar");
+    
+});
+
+body.addEventListener('click', e =>{
+    let clickedElm = e.target;
+
+    if (!clickedElm.classList.contains('openMenu') && !clickedElm.classList.contains('menu')) {
+        nav.classList.remove('activar');
+    }
+});
+
+//JS CODE BUTTON EFECT
+document.querySelectorAll('.button').forEach(button => {
+
+    let duration = 3000,
+        svg = button.querySelector('svg'),
+        svgPath = new Proxy({
+            y: null,
+            smoothing: null
+        }, {
+            set(target, key, value) {
+                target[key] = value;
+                if(target.y !== null && target.smoothing !== null) {
+                    svg.innerHTML = getPath(target.y, target.smoothing, null);
+                }
+                return true;
+            },
+            get(target, key) {
+                return target[key];
+            }
+        });
+
+    button.style.setProperty('--duration', duration);
+
+    svgPath.y = 20;
+    svgPath.smoothing = 0;
+
+    button.addEventListener('click', e => {
+        
+        e.preventDefault();
+
+        if(!button.classList.contains('loading')) {
+
+            button.classList.add('loading');
+
+            gsap.to(svgPath, {
+                smoothing: .3,
+                duration: duration * .065 / 1000
+            });
+
+            gsap.to(svgPath, {
+                y: 12,
+                duration: duration * .265 / 1000,
+                delay: duration * .065 / 1000,
+                ease: Elastic.easeOut.config(1.12, .4)
+            });
+
+            setTimeout(() => {
+                svg.innerHTML = getPath(0, 0, [
+                    [3, 14],
+                    [8, 19],
+                    [21, 6]
+                ]);
+            }, duration / 2);
+
+        }
+
+    });
+
+});
+
+function getPoint(point, i, a, smoothing) {
+    let cp = (current, previous, next, reverse) => {
+            let p = previous || current,
+                n = next || current,
+                o = {
+                    length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
+                    angle: Math.atan2(n[1] - p[1], n[0] - p[0])
+                },
+                angle = o.angle + (reverse ? Math.PI : 0),
+                length = o.length * smoothing;
+            return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
+        },
+        cps = cp(a[i - 1], a[i - 2], point, false),
+        cpe = cp(point, a[i - 1], a[i + 1], true);
+    return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
+}
+
+function getPath(update, smoothing, pointsNew) {
+    let points = pointsNew ? pointsNew : [
+            [4, 12],
+            [12, update],
+            [20, 12]
+        ],
+        d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
+    return `<path d="${d}" />`;
+}
+
+
+
+
